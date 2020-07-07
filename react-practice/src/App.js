a7d533c4ae64fde56bfc60cf1b8786ed3faaa563
dae-hyun-kim <daehk4@uci.edu>
2020-07-07 22:09:13 +0000

    create count with buttons

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./routes/Home"
--- react-practice/src/routes/About.js
+++ react-practice/src/routes/About.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const About = () => {
+
+  const [count, setCount] = useState(0)
+
+  console.log(count)
   return (
+    <>
     <h3>ABOUT PAGE</h3>
+      <div>
+        <button> - </button>
+        {count}
+        <button> + </button>
+      </div>
+    </>
   )
 }
 
